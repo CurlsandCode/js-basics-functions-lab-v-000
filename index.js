daec6b39f837555e0ca2a1453f730799f1746974
Monika Williams <monika.m.williams@gmail.com>
2017-12-03 11:33:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ function distanceTravelledInFeet(startblock, endblock) {
      return (startblock- endblock) * 264;
    }
 }
+
+function calculatesFarePrice(start, destination) {
+  distance = distanceTravelledInFeet(start, destination)
+  if (distance <= 400) {
+    return 0;
+  } else if (distance> 400 &&  distance <= 2000) {
+    return distance * 0.02;
+  }
+}
